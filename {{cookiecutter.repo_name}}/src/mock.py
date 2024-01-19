8cadfac1bb1cf9b70370bbe278caad024abae952
Jose R. Zapata <jjrzg@hotmail.com>
2024-01-19 14:59:07 -0500

    🤡 Add mocking test

---
+++ {{cookiecutter.repo_name}}/src/mock.py
@@ -0,0 +1,7 @@
+def mocking_test() -> bool:
+    """
+    This function is for making pytest run.
+
+    Delete this function after first python script with code is created.
+    """
+    return True
---
+++ {{cookiecutter.repo_name}}/tests/test_mock.py
@@ -0,0 +1,10 @@
+from mock import mocking_test
+
+
+def test_mocking_test() -> None:
+    """
+    Test function for mocking_test() function.
+
+    Asserts that the mocking_test() function returns True.
+    """
+    assert mocking_test() is True
