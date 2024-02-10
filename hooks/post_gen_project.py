90bf67fb4b50e1a56d1fe3fc9b03db53afdc2355
Jose R. Zapata <jjrzg@hotmail.com>
2024-02-09 22:39:35 -0500

    🐛 🍪 fix mkdocs options

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -20,6 +20,7 @@ mkdocs = '{% if cookiecutter.mkdocs != "y" %}n{% endif %}'
 codecov = '{% if cookiecutter.codecov != "y" %}n{% endif %}'
 
 if __name__ == "__main__":
+    print(mkdocs)
     if mkdocs != "y":
         remove_dir("docs")
         remove_file("mkdocs.yml")
