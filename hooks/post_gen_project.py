27db8c9853764be53eb3a7645faa5cd5a8660749
Jose R. Zapata <jjrzg@hotmail.com>
2024-02-10 13:30:25 -0500

    🔧  🍪 add scripts to init git repo

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,23 +1,34 @@
 #!/usr/bin/env python
 import os
 import shutil
+import subprocess  # nosec
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
 
 def remove_file(filepath: str) -> None:
+    """Remove file from project directory.
+
+    Args:
+        filepath (str): File path relative to project directory.
+    """
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
 
 
 def remove_dir(filepath: str) -> None:
+    """Remove directory from project directory.
+
+    Args:
+        filepath (str): Directory path relative to project directory.
+    """
     shutil.rmtree(os.path.join(PROJECT_DIRECTORY, filepath))
 
 
-mkdocs = "{% if cookiecutter.mkdocs %}y{% endif %}"
-codecov = "{% if cookiecutter.codecov%}y{% endif %}"
-
 if __name__ == "__main__":
-    print(mkdocs)
+    mkdocs = "{% if cookiecutter.mkdocs %}y{% endif %}"
+    codecov = "{% if cookiecutter.codecov%}y{% endif %}"
+    init_git = "{% if cookiecutter.init_git %}y{% endif %}"
+
     if mkdocs != "y":
         remove_dir("docs")
         remove_file("mkdocs.yml")
@@ -25,3 +36,11 @@ if __name__ == "__main__":
 
     if codecov != "y":
         remove_file("codecov.yml")
+
+    if init_git != "y":
+        # init git repo
+        subprocess.run(["git", "init", "-b", "main"], check=False)  # nosec
+        subprocess.run(["git", "add", "."], check=False)  # nosec
+        subprocess.run(
+            ["git", "commit", "-m", "🎉 Begin a project, Initial commit"], check=False
+        )  # nosec
