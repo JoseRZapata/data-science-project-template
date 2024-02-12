7e0ecb520fe76c1c724a1fa08decfd9241af190b
Jose R. Zapata <JoseRZapata@users.noreply.github.com>
2024-02-12 10:12:44 -0500

    Feature/check_python (#26)
    * 🍪 💚 fix action yml automatic python selection
    
    * 👷 upgrade to 3.11
    
    * ✅ add test to check python version
    
    * ♻️ ✅  refactor test to no repeat code

---
+++ tests/test_create_template_python_version.py
@@ -0,0 +1,19 @@
+def test_cookiecutter_python_version(cookies) -> None:  # type: ignore
+    """check correct python version in the generated files."""
+    versions = ["3.10", "3.11"]
+    file_paths = [
+        ".github/actions/python-poetry-env/action.yml",
+        ".github/workflows/test.yml",
+        "pyproject.toml",
+    ]
+
+    for python_ver in versions:
+        result = cookies.bake(extra_context={"compatible_python_versions": python_ver})
+
+        for file_path in file_paths:
+            env_path = result.project_path / file_path
+            assert env_path.is_file()
+
+            with open(env_path) as f:
+                file_content = f.read()
+                assert python_ver in file_content
