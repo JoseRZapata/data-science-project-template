21749471abd28ba533530c4c5c2d7f7aa081452f
Jose R. Zapata <jjrzg@hotmail.com>
2024-02-12 14:48:25 -0500

    🔧 Remove mkdocs inf if is not needed

--- tests/test_create_template_no_mkdocs.py
+++ tests/test_create_template_no_mkdocs.py
@@ -13,9 +13,12 @@ def test_cookiecutter_mkdocs_files(cookies) -> None:  # type: ignore
         env_path = result.project_path / file_path
         assert not env_path.is_file()
 
-    env_path = result.project_path / "pyproject.toml"
-    assert env_path.is_file()
+    file_paths = ["pyproject.toml", "Makefile"]
+    text_check = ["tool.poetry.group.docs.dependencies", "mkdocs"]
 
-    with open(env_path) as f:
-        file_content = f.read()
-        assert "tool.poetry.group.docs.dependencies" not in file_content
+    for file_path, text in zip(file_paths, text_check, strict=False):
+        env_path = result.project_path / file_path
+        if env_path.is_file():
+            with open(env_path) as f:
+                file_content = f.read()
+                assert text not in file_content
